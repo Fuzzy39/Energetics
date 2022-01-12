16b4005245225ec2d9fcf0e01877cbd42e4d7b69
Matthew R Lewis <2023lewismr@sa.hccsc.us>
2022-01-12 14:15:04 -0500

    I added forms

---
+++ Energetics/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Energetics
+{
+    public partial class Form2 : Form
+    {
+        Form1 mainForm;
+
+        public Form2(Form1 creator)
+        {
+            InitializeComponent();
+            mainForm = creator;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
