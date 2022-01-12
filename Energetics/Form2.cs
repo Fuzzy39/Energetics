0b5d678a2ec9138b765679c3ac0de9d8723f1740
Mason S Hill <2023hillms@sa.hccsc.us>
2022-01-12 15:23:11 -0500

    programmatically added control

--- Energetics/Form2.cs
+++ Energetics/Form2.cs
@@ -17,12 +17,36 @@ namespace Energetics
         public Form2(Form1 creator)
         {
             InitializeComponent();
+
+          
             mainForm = creator;
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            
+
+
+            Label test = new Label();
+            test.ForeColor = Color.Black;
+            // test.BackColor = Color.Black;
+            test.Location = new Point(100, 100);
+            test.Text = "Fart sandwich";
+            test.Size = new Size(100, 100);
+            test.Visible = true;
+            //this.PerformLayout();
+            // this.Text = "why u no work?";
+            this.Controls.Add(test);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // mainForm.Visible = true;
+            mainForm.Close();
         }
     }
 }
