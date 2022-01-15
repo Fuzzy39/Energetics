7c4434d15a3259667ddc035da1d864ac3da89f28
Mason S Hill <2023hillms@sa.hccsc.us>
2022-01-14 21:14:03 -0500

    minesweeperTile more or less complete

--- Energetics/MinesweeperTile.cs
+++ Energetics/MinesweeperTile.cs
@@ -24,24 +24,29 @@ namespace Energetics
 
 
         private bool isHovered = false;
-        private int hoverFrame =0;
-        private const int hoverCap = 50;
-        private double hoverspeed = 5;
+        private int brightness =0;
+        private const int hoverBrightnessTarget = 50;
+        private int brightnessChange = 0;
+       
 
         static private Random rand = new Random();
         private Point gridLocation;
         private int neighbors;
         private bool revealed;
+        private bool flagged;
 
         public MinesweeperTile(int size, Point gridLocation, Point location, int neighbors)
         {
-            baseReds = new int[] { 55, 230, 135 };
-            baseGreens = new int[]{ 135, 20, 135};
-            baseBlues = new int[] { 55, 20, 135 };
+
+            //                      u    X    0    1    2    3    4    5    6    7    8
+            baseReds = new int[]  { 55,  230, 135, 40,  70,  100, 130, 160, 160, 160, 160};
+            baseGreens = new int[]{ 135, 20,  135, 40,  70,  100, 130, 160, 120, 80,  40};
+            baseBlues = new int[] { 55,  20,  135, 160, 130, 100, 70,  40,  40,  40,  40};
             this.gridLocation = gridLocation;
             this.Location = location;
             this.neighbors = neighbors;
             revealed = false;
+            flagged = false;
 
             
             InitializeComponent();
@@ -80,32 +85,12 @@ namespace Energetics
 
             }
 
-            // make color brighter (or darker) based on hover state
-            if(isHovered)
-            {
-             
-                hoverFrame += (int)hoverspeed;
-                if (hoverFrame > hoverCap)
-                {
-                    hoverFrame = hoverCap;
-                }
+            determineBrightness();
+            red += brightness;
+            green += brightness;
+            blue += brightness;
 
-                // add this 'frame's' effect
-                red += hoverFrame;
-                green += hoverFrame;
-                blue += hoverFrame;
 
-                // tween hover effects
-                if (hoverFrame <= hoverCap * .5)
-                {
-                    hoverspeed *= 1.7;
-                }
-                else
-                {
-                    hoverspeed *= 1.0/1.7;
-                }
-            }
-           
             // add jitter.
             int jitter = (int)(colorRange*Math.Sin(colorFrame))+colorRange/2;
 
@@ -118,14 +103,28 @@ namespace Energetics
                 switch(neighbors)
                 {
                     case -1:
-                        red += jitter;
+                    case 6:
+                    case 7:
+                    case 8:
+                        red += 2*jitter;
                         break;
                     case 0:
                         red += jitter;
                         green += jitter;
                         blue += jitter;
-
                         break;
+                   case 1:
+                    case 2:
+                        blue += 2*jitter;
+                        break;
+                    
+                    case 4:
+                    case 5:
+                   
+                        green += jitter;
+                        red += jitter;
+                        break;
+
                 }
             }
             //red += jitter;
@@ -154,33 +153,61 @@ namespace Energetics
         private void lblTile_MouseEnter(object sender, EventArgs e)
         {
             isHovered = true;
-            hoverFrame = 10;
-            hoverspeed = 5;
+           // hoverFrame = 10;
+            //hoverspeed = 5;
         }
 
         private void lblTile_MouseLeave(object sender, EventArgs e)
         {
             isHovered = false;
-            hoverFrame = 0;
-            hoverspeed = 5;
+           // hoverFrame = 0;
+          // hoverspeed = 5;
         }
 
         private void lblTile_Click(object sender, EventArgs e)
         {
             // can you detect a right click?
-            // eh, whatever.
-            hoverFrame = -30;
-            hoverspeed = 4;
-            reveal();
+            // yes!
+            MouseEventArgs me = (MouseEventArgs)e;
+            if (me.Button == MouseButtons.Right)
+            { 
+                if(!revealed)
+                {
+                    brightness = 90;
+                    flagged = !flagged;
+                    if (flagged)
+                    {
+                        lblTile.Text = "⚑";
+                        
+                    }
+                    else
+                    {
+                        lblTile.Text = "";
+                    }
+
+                }
+            }
+            if (me.Button == MouseButtons.Left) 
+            {
+                if (!revealed & !flagged)
+                {
+                    reveal();
+                }
+            }
+            
             //MessageBox.Show("You clicked at tile (" + gridLocation.X + ", " + gridLocation.Y + ").");
         }
 
         private void reveal()
         {
+            
+            brightness = 150;
+            brightnessChange = 0;
             revealed = true;
+           
             if(neighbors==-1)
             {
-                lblTile.Text = "X";
+                lblTile.Text = "💣";
                 return;
             }
             if(neighbors==0)
@@ -189,6 +216,149 @@ namespace Energetics
             }
             lblTile.Text = "" + neighbors;
         }
+
+        private void determineBrightness()
+        {
+            // Okay, I've been a bit silly.
+            // first, the tile determines it's target brightness, and how far away it is from that
+            // brightness.
+            // There are 3 phases of speed, ease in, steady, and ease out.
+            // make color brighter (or darker) based on hover state
+
+            int steadySpeed = 15;
+            int easeRate = 3;
+
+            int target;
+            int distance;
+
+            if(isHovered)
+            {
+                target = hoverBrightnessTarget;
+              
+            }
+            else
+            {
+                target = 0;
+            }
+
+            distance = target - brightness;
+            bool darkening = distance < 0;
+            // if nothing needs to be done, do nothing
+            if (distance == 0) return;
+
+            // determine the change to be made.
+            if(darkening)
+            {
+                if(brightnessChange > steadySpeed*-1)
+                {
+                    brightnessChange -= easeRate;
+                }
+                else
+                {
+                    brightnessChange = steadySpeed * -1;
+                }
+            }
+            else
+            {
+                if (brightnessChange < steadySpeed)
+                {
+                    brightnessChange += easeRate;
+                }
+                else
+                {
+                    brightnessChange = steadySpeed;
+                }
+            }
+
+            // change the brightness!
+            brightness += brightnessChange;
+            if(darkening)
+            {
+                if (brightness<=target)
+                {
+                    brightnessChange = 0;
+                    brightness = target;
+                }
+            }
+            else
+            {
+                if(brightness>=target)
+                {
+                    brightnessChange = 0;
+                    brightness = target;
+                }
+            }
+
+
+
+            /*
+            if (isHovered)
+            {
+                // we need to figure out the correct speed and apply it.
+                target = hoverBrightnessTarget;
+                distance = hoverBrightnessTarget - brightness;
+                         
+            }
+            else
+            {
+                // target is 0
+                target = 0;
+                distance = brightness;
+            }
+
+           // lblTile.Text = distance + "";
+            // based on the distance from our target, we can set our exponent.
+            // if the distance is half of the hover brightness target, the speed will be reduced.
+            if (distance == 0)
+            {
+                return;
+            }
+            
+
+            int brightSpeed = 0;
+
+            if *Math.Abs(distance) < hoverBrightnessTarget / 2true)
+            {
+                // brightness speed is accellerating
+                int distanceCovered = target - distance;
+                brightSpeed = (int)(HoverBaseSpeed * Math.Pow(HoverSpeedExponent, distanceCovered));
+            }
+            else
+            {
+                // brightness speed is decreasing, so we need to add another term.
+                int midPoint = hoverBrightnessTarget / 2;
+                int distanceCovered = Math.Abs(hoverBrightnessTarget - distance - midPoint);
+                int maxSpeed = (int)(HoverBaseSpeed * Math.Pow(HoverSpeedExponent, midPoint));
+                brightSpeed = maxSpeed - (int)(HoverBaseSpeed * Math.Pow(1.0/HoverSpeedExponent, distanceCovered));
+            }
+            
+            brightness += brightSpeed;
+            */
+            /*hoverFrame += (int)hoverspeed;
+            if (hoverFrame > hoverCap)
+            {
+                hoverFrame = hoverCap;
+                //hoverspeed *= -1;
+            }
+
+            // add this 'frame's' effect
+            red += hoverFrame;
+            green += hoverFrame;
+            blue += hoverFrame;
+
+            // tween hover effects
+            if (hoverFrame <= hoverCap * .5)
+            {
+                hoverspeed *= 1.7;
+            }
+            else
+            {
+                hoverspeed *= 1.0 / 1.7;
+            }*/
+
+
+        }   
+            
     }
 
 }
